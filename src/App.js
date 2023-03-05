92fafa38c88a18ee5deb9b8e46dbe0033ba47a31
kamayatu <sarubeji15@gmail.com>
2023-03-05 16:57:11 +0900

    complete htmlCss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import "./style.css"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="input-area">
+      <input placeholder="todoを入力" />
+      <button className="button">追加</button>
     </div>
+    <div className="incomplete-area">
+      <p className="title">未完了リスト</p>
+      <ul>
+        <li>
+          <div className="list-row">
+          <p>項目</p>
+          <button className="button">編集</button>
+          <button className="button">完了</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div className="complete-area">
+      <p className="title">完了リスト</p>
+      <ul>
+        <li>
+          <div className="list-row">
+          <p>項目</p>
+          <button className="button">戻す</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+    </>
   );
-}
+};
 
 export default App;
