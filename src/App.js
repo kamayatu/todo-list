187d0a3fe20cc9fb08b3f4c468be248ea8c681a2
kamayatu <sarubeji15@gmail.com>
2023-03-05 17:42:42 +0900

    add Add func

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css"
 function App() {
+  const [incompleteTodos, setIncompleteTodos] = useState(['ああああ','いいいい']);
+  const [completeTodos, setCompleteTodos] = useState(['うううう']);
+  const [todoText, setTodoText] = useState('');
+
+  const onChangeTodoText = (event) => {
+    setTodoText(event.target.value)
+  };
+
+  const addNewTask = () => {
+    if(todoText !== ''){
+    const newTodos = [...incompleteTodos, todoText]
+    setIncompleteTodos(newTodos);
+    setTodoText('');
+  }};
+
   return (
     <>
     <div className="input-area">
-      <input placeholder="todoを入力" />
-      <button className="button">追加</button>
+      <input value={todoText} onChange={onChangeTodoText} placeholder="todoを入力" />
+      <button className="button" onClick={addNewTask}>追加</button>
     </div>
+
     <div className="incomplete-area">
       <p className="title">未完了リスト</p>
       <ul>
-        <li>
+      {incompleteTodos.map((todo) => {
+        return(
+          <li key={todo}>
           <div className="list-row">
-          <p>項目</p>
+          <p>{todo}</p>
           <button className="button">編集</button>
           <button className="button">完了</button>
           </div>
         </li>
+        );
+      })}       
       </ul>
     </div>
     <div className="complete-area">
       <p className="title">完了リスト</p>
       <ul>
-        <li>
-          <div className="list-row">
-          <p>項目</p>
-          <button className="button">戻す</button>
-          </div>
-        </li>
+        {completeTodos.map((todo) => {
+          return(
+            <li key={todo}>
+            <div className="list-row">
+            <p>{todo}</p>
+            <button className="button">戻す</button>
+            </div>
+          </li>
+          );
+        })}
       </ul>
     </div>
     </>
